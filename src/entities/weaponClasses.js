163559f53bc05180ee7b71543a7e5021aa853cc0
Hunter Quant <hunterdquant@gmail.com>
2016-10-24 08:46:21 -0400

    Base weapon class complete

---
+++ src/entities/weaponClasses.js
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////Weapons///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+//Weapon Class
+//Base of All Weapons, not directly used
+//Extension of Entity
+class Weapon {
+  constructor(x, y, texture, shoots, ammo){
+    super(x, y, texture);
+    this.x = x;
+    this.y = y;
+    this.rotation = 0;
+    this.sprite = new Sprite(texture);
+    this.sprite.x = this.x;
+    this.sprite.y = this.y;
+    this.shoots = shoots;
+    this.ammo = ammo;
+  }
+
+  /* Spawns a projectile and fires in the direction of the input vector */
+  fire(vector) {
+
+  }
+
+  /* Updates the rotation and position of the weapon around the player */
+  update(vector) {
+
+  }
+
+  /* Updates weapons sprite */
+  render() {
+
+  }
+}
+
+//Max Heap Blunderbuss Class
+//Extension of Weapon
+class MaxHeapBlunderbuss extends Weapon {
+  constructor(x, y, texture, ammo) {
+    super(x, y, texture, "Spread", ammo);
+    console.log("Max Heap Blunderbuss Created");
+  }
+}
+
+//Key Value Duals Class
+//Extension of Weapon
+class KeyValueDuals extends Weapon {
+  constructor(x, y, texture, ammo) {
+    super(x, y, texture, "Bullets", ammo);
+    console.log("Key Value Duals Created");
+  }
+}
+
+//Recursion Rifle Class
+//Extension of Weapon
+class RecursionRifle extends Weapon {
+  constructor(x, y, texture, ammo) {
+    super(x, y, texture, "Beam", ammo);
+    console.log("Recursion Rifle Created");
+  }
+}
